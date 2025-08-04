3a81aa77a7856f71507fcebb174c7d8748fd8fbf
Hema0501 <r.hemapriya2007@gmail.com>
2025-08-04 14:51:14 +0530

    Update Main.java
    PROGRAM : 1

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+//PROGRAM:1
+//TEST CASE:1 SUM OF 1 TO 100
 package oops;
 public class Main {
 	public static void main(String[] args) {
@@ -9,5 +11,77 @@ public class Main {
 	}
 }
 
-Output:
+//OUTPUT:
 The sum of 1 to 100 is 5050
+
+//TEST CASE:2 SUM OF 1 TO 50
+package oops;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=1;i<=50;i++) {
+			sum+=i;
+		}
+		System.out.println("The sum of 1 to 50 is "+sum);
+
+	}
+}
+
+//OUTPUT:
+The sum of 1 to 50 is 1275
+
+//TEST CASE:3 SUM OF 1 TO 10
+package oops;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=1;i<=10;i++) {
+			sum+=i;
+		}
+		System.out.println("The sum of 1 to 10 is "+sum);
+
+	}
+}
+
+//OUTPUT:
+The sum of 1 to 10 is 55
+
+//TEST CASE:4 SUM OF 50 to 100
+package oops;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=50;i<=100;i++) {
+			sum+=i;
+		}
+		System.out.println("The sum of 50 to 100 is "+sum);
+
+	}
+}
+
+//OUTPUT:
+The sum of 50 to 100 is 3825
+
+//TEST CASE:5 SUM OF 1 to 1
+package oops;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=1;i<=1;i++) {
+			sum+=i;
+		}
+		System.out.println("The sum of 1 to 1 is "+sum);
+
+	}
+}
+
+//OUTPUT:
+The sum of 1 to 1 is 1
