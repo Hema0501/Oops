17bb7400f24c431d2205f2a0ec6101d49df83879
Hema0501 <r.hemapriya2007@gmail.com>
2025-08-04 14:39:02 +0530

    Create Main.java
    It is the exercise 1.

---
+++ Main.java
@@ -0,0 +1,13 @@
+package oops;
+public class Main {
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=1;i<=100;i++) {
+			sum+=i;
+		}
+		System.out.println("The sum of 1 to 100 is "+sum);
+	}
+}
+
+Output:
+The sum of 1 to 100 is 5050
