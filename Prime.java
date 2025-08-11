e6828f4c03a3727455d0405ff525c3c98f5ea367
Hema0501 <r.hemapriya2007@gmail.com>
2025-08-11 22:08:56 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,209 @@
+// PROGRAM:2
+//TC:1 Input 2 > Output:Prime
+package oops;
+import java.util.Scanner;
+public class prime{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num,i,count=0;
+		System.out.println("HEMAPRIYA R");
+		System.out.println("2117240070115");
+		System.out.println("Enter a number:");
+		num=sc.nextInt();
+			if (num<=1) {
+				System.out.println("It is not a Prime");
+			}
+			else {
+				for(i=2;i<=Math.sqrt(num);i++) {
+					if(num%i==0) {
+						count=1;
+						break;
+					}
+				}
+				if(count==0) {
+					System.out.println("It is a Prime");
+				}
+				else {
+					System.out.println("It is a not Prime");
+				}
+			}
+	}
+}
+
+//OUTPUT:
+HEMAPRIYA R
+2117240070115
+Enter a number:
+2
+It is a Prime
+
+
+//TC:2 Input 15 > Output:Not Prime
+package oops;
+import java.util.Scanner;
+public class prime{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num,i,count=0;
+		System.out.println("HEMAPRIYA R");
+		System.out.println("2117240070115");
+		System.out.println("Enter a number:");
+		num=sc.nextInt();
+			if (num<=1) {
+				System.out.println("It is not a Prime");
+			}
+			else {
+				for(i=2;i<=Math.sqrt(num);i++) {
+					if(num%i==0) {
+						count=1;
+						break;
+					}
+				}
+				if(count==0) {
+					System.out.println("It is a Prime");
+				}
+				else {
+					System.out.println("It is a not Prime");
+				}
+			}
+	}
+}
+
+//OUTPUT:
+HEMAPRIYA R
+2117240070115
+Enter a number:
+15
+It is a not Prime
+
+
+
+//TC:3 Input 17 > Output:Prime
+package oops;
+import java.util.Scanner;
+public class prime{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num,i,count=0;
+		System.out.println("HEMAPRIYA R");
+		System.out.println("2117240070115");
+		System.out.println("Enter a number:");
+		num=sc.nextInt();
+			if (num<=1) {
+				System.out.println("It is not a Prime");
+			}
+			else {
+				for(i=2;i<=Math.sqrt(num);i++) {
+					if(num%i==0) {
+						count=1;
+						break;
+					}
+				}
+				if(count==0) {
+					System.out.println("It is a Prime");
+				}
+				else {
+					System.out.println("It is a not Prime");
+				}
+			}
+	}
+}
+
+//OUTPUT:
+HEMAPRIYA R
+2117240070115
+Enter a number:
+17
+It is a Prime
+
+
+
+//TC:4 Input 1 > Output:Not Prime
+package oops;
+import java.util.Scanner;
+public class prime{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num,i,count=0;
+		System.out.println("HEMAPRIYA R");
+		System.out.println("2117240070115");
+		System.out.println("Enter a number:");
+		num=sc.nextInt();
+			if (num<=1) {
+				System.out.println("It is not a Prime");
+			}
+			else {
+				for(i=2;i<=Math.sqrt(num);i++) {
+					if(num%i==0) {
+						count=1;
+						break;
+					}
+				}
+				if(count==0) {
+					System.out.println("It is a Prime");
+				}
+				else {
+					System.out.println("It is a not Prime");
+				}
+			}
+	}
+}
+
+//OUTPUT:
+HEMAPRIYA R
+2117240070115
+Enter a number:
+1
+It is a not Prime
+
+
+
+
+//TC:5 Input 0 > Output:Not Prime
+package oops;
+import java.util.Scanner;
+public class prime{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num,i,count=0;
+		System.out.println("HEMAPRIYA R");
+		System.out.println("2117240070115");
+		System.out.println("Enter a number:");
+		num=sc.nextInt();
+			if (num<=1) {
+				System.out.println("It is not a Prime");
+			}
+			else {
+				for(i=2;i<=Math.sqrt(num);i++) {
+					if(num%i==0) {
+						count=1;
+						break;
+					}
+				}
+				if(count==0) {
+					System.out.println("It is a Prime");
+				}
+				else {
+					System.out.println("It is a not Prime");
+				}
+			}
+	}
+}
+
+//OUTPUT:
+HEMAPRIYA R
+2117240070115
+Enter a number:
+0
+It is a not Prime
+
+
+
+
+
+
+
+
+
+
+
