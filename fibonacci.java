34738303dd131295781196e2f02301be51aa68a2
Hema0501 <r.hemapriya2007@gmail.com>
2025-08-11 22:39:09 +0530

    Create fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,167 @@
+// PROGRAM: 3
+// TC1: Input n=5 > Output:0 1 1 2 3
+package oops;
+import java.util.Scanner;
+public class fibonacci {
+    static int fib(int n) {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return fib(n - 1) + fib(n - 2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("HEMAPRIYA R");
+		    System.out.println("2117240070115");
+        System.out.print("Enter the n: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib(i) + " ");
+        }
+        sc.close();
+    }
+}
+
+//OUTPUT:
+HEMAPRIYA R
+2117240070115
+Enter the n: 5
+0 1 1 2 3 
+
+
+
+// TC2: Input n=7 > Output:0 1 1 2 3 5 8
+package oops;
+import java.util.Scanner;
+public class fibonacci {
+    static int fib(int n) {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return fib(n - 1) + fib(n - 2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("HEMAPRIYA R");
+		    System.out.println("2117240070115");
+        System.out.print("Enter the n: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib(i) + " ");
+        }
+        sc.close();
+    }
+}
+
+//OUTPUT:
+HEMAPRIYA R
+2117240070115
+Enter the n: 7
+0 1 1 2 3 5 8
+
+
+
+// TC3: Input n=1 > Output:0 
+package oops;
+import java.util.Scanner;
+public class fibonacci {
+    static int fib(int n) {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return fib(n - 1) + fib(n - 2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("HEMAPRIYA R");
+		    System.out.println("2117240070115");
+        System.out.print("Enter the n: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib(i) + " ");
+        }
+        sc.close();
+    }
+}
+
+//OUTPUT:
+HEMAPRIYA R
+2117240070115
+Enter the n: 1
+0 
+
+
+
+// TC4: Input n=0 > Output:
+package oops;
+import java.util.Scanner;
+public class fibonacci {
+    static int fib(int n) {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return fib(n - 1) + fib(n - 2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("HEMAPRIYA R");
+		    System.out.println("2117240070115");
+        System.out.print("Enter the n: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib(i) + " ");
+        }
+        sc.close();
+    }
+}
+
+//OUTPUT:
+HEMAPRIYA R
+2117240070115
+Enter the n: 0
+
+
+
+
+// TC5: Input n=10 > Output:0 1 1 2 3 5 8 13 21 34 
+package oops;
+import java.util.Scanner;
+public class fibonacci {
+    static int fib(int n) {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return fib(n - 1) + fib(n - 2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("HEMAPRIYA R");
+		    System.out.println("2117240070115");
+        System.out.print("Enter the n: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib(i) + " ");
+        }
+        sc.close();
+    }
+}
+
+//OUTPUT:
+HEMAPRIYA R
+2117240070115
+Enter the n: 10
+0 1 1 2 3 5 8 13 21 34  
+
+
+
+
